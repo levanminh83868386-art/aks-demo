840be5666126d568ad4466a43bc3c60c90c34687
lvanminh <minhle@ntdatateam.onmicrosoft.com>
2025-10-23 10:42:29 +0700

    ffffffffffff

--- modules/vnet_and_subnets/main.tf
+++ modules/vnet_and_subnets/main.tf
@@ -13,8 +13,8 @@ terraform {
 # official documentation https://registry.terraform.io/providers/hashicorp/azurerm/3.65.0/docs/resources/virtual_network
 resource "azurerm_virtual_network" "minh_az_vnet" {
   name                = var.vnet_name
-  location            = azurerm_resource_group.myTFResourceGroup.location
-  resource_group_name = azurerm_resource_group.myTFResourceGroup.name
+  location            = "northeurope"
+  resource_group_name = "myTFResourceGroup"
   address_space       = var.address_space
   tags                = merge(var.tags, var.additional_vnet_tags)
 }
@@ -23,7 +23,7 @@ resource "azurerm_virtual_network" "minh_az_vnet" {
 # official documentation https://registry.terraform.io/providers/hashicorp/azurerm/3.65.0/docs/resources/subnet
 resource "azurerm_subnet" "minh_az_subnet" {
   name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.myTFResourceGroup.name
+  resource_group_name  = "myTFResourceGroup"
   virtual_network_name = azurerm_virtual_network.minh_az_vnet.name
   address_prefixes     = var.subnet_address_prefix
   service_endpoints    = var.service_endpoints
