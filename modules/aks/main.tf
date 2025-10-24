5da0ee75538643cd45b9e789eaf8f9ab038b7772
lvanminh <minhle@ntdatateam.onmicrosoft.com>
2025-10-24 14:01:31 +0700

    f

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -65,9 +65,23 @@ resource "azurerm_kubernetes_cluster_node_pool" "k8s-worker" {
   node_labels = each.value.node_labels
 }
 
+resource "azurerm_container_registry" "minhtestacr" {
+  name                = "minhtestacr"
+  resource_group_name = var.resource_group_name
+  location            = var.region
+  sku                 = "Standard"
+}
+
+# resource "azurerm_role_assignment" "minh_aks_acr_pull" {
+#   principal_id                     = "9e5f1414-9e67-4501-9725-7004b067ef7b"
+#   role_definition_name             = "AcrPull"
+#   scope                            = "/subscriptions/37b25916-1e2d-4883-bb99-00f8f55f2a88/resourceGroups/myTFResourceGroup/providers/Microsoft.ContainerRegistry/registries/minhtestacr"
+#   skip_service_principal_aad_check = true
+# }
+
 resource "azurerm_role_assignment" "minh_aks_acr_pull" {
-  principal_id                     = "9e5f1414-9e67-4501-9725-7004b067ef7b"
+  principal_id                     = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
-  scope                            = "/subscriptions/37b25916-1e2d-4883-bb99-00f8f55f2a88/resourceGroups/myTFResourceGroup/providers/Microsoft.ContainerRegistry/registries/minhtestacr"
+  scope                            = azurerm_container_registry.minhtestacr.id
   skip_service_principal_aad_check = true
 }
