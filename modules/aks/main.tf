42ae442f6b1be23548446dbbcd51037957f46368
lvanminh <minhle@ntdatateam.onmicrosoft.com>
2025-10-24 09:45:49 +0700

    sss

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -64,3 +64,10 @@ resource "azurerm_kubernetes_cluster_node_pool" "k8s-worker" {
 
   node_labels = each.value.node_labels
 }
+
+resource "azurerm_role_assignment" "minh_aks_acr_pull" {
+  principal_id                     = "9e5f1414-9e67-4501-9725-7004b067ef7b"
+  role_definition_name             = "AcrPull"
+  scope                            = "/subscriptions/37b25916-1e2d-4883-bb99-00f8f55f2a88/resourceGroups/myTFResourceGroup/providers/Microsoft.ContainerRegistry/registries/minhtestacr"
+  skip_service_principal_aad_check = true
+}
