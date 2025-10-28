f6f819f52ec25cd6d065eb24fa03db9ea854da11
lvanminh <minh.le@icariohealth.com>
2025-10-28 22:07:58 +0700

    fghjfghjhfgj

--- modules/monitoring/providers.tf
+++ modules/monitoring/providers.tf
@@ -1,18 +1,18 @@
 # providers.tf
 # Configure the Kubernetes Provider
 provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.k8s.kube_config[0].host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate)
+  host                   = var.host
+  client_certificate     = base64decode(var.client_certificate)
+  client_key             = base64decode(var.client_key)
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
 }
 
 # Configure the Helm Provider
 provider "helm" {
   kubernetes = {
-    host                   = azurerm_kubernetes_cluster.k8s.kube_config[0].host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate)
+    host                   = var.host
+    client_certificate     = base64decode(var.client_certificate)
+    client_key             = base64decode(var.client_key)
+    cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
   }
 }
