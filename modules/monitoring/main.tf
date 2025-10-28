a4fa064a305a278c9c9431e38bcb86d6bd94da08
lvanminh <minh.le@icariohealth.com>
2025-10-28 21:39:45 +0700

    srfghstrhstrhrwhjj

---
+++ modules/monitoring/main.tf
@@ -0,0 +1,28 @@
+# monitoring.tf - Deploy the Helm chart
+resource "helm_release" "kube_prometheus_stack" {
+  # Helm repository details
+  name       = "kube-prometheus-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = "59.0.0" # Use a stable, recent version
+  namespace  = "monitoring"
+  create_namespace = true
+  
+   set {
+    name  = "grafana.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "grafana.adminPassword"
+    value = "admin123"
+  }
+
+  set {
+    name  = "prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues"
+    value = "false"
+  }
+  # Optional: Wait for all resources to be ready (can take a few minutes)
+  timeout = 600
+  wait    = true
+}
