85644ba3ad5fdee1cac5f6b30cd41b987fdde343
lvanminh <minh.le@icariohealth.com>
2025-10-28 22:31:36 +0700

    asdasdsa

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -8,6 +8,24 @@ output "client_certificate" {
   value       = azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate
   sensitive   = true
 }
+output "host" {
+  description = "Kubernetes cluster host."
+  value       = azurerm_kubernetes_cluster.k8s.kube_config.0.host
+  sensitive   = true
+}
+
+output "client_key" {
+  description = "Base64 encoded public client_key."
+  value       = azurerm_kubernetes_cluster.k8s.kube_config.0.client_key
+  sensitive   = true
+}
+
+output "cluster_ca_certificate" {
+  description = "Base64 encoded public cluster_ca_certificate."
+  value       = azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate
+  sensitive   = true
+}
+
 
 output "kube_config" {
   description = "Raw Kubernetes config to be used by kubectl and other compatible tools."
