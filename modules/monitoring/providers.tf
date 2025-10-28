932fa08f0e7d3ad01c8fd2320717eb9e2e9b47f7
lvanminh <minh.le@icariohealth.com>
2025-10-28 21:52:32 +0700

    ds

--- modules/monitoring/providers.tf
+++ modules/monitoring/providers.tf
@@ -9,7 +9,7 @@ provider "kubernetes" {
 
 # Configure the Helm Provider
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = azurerm_kubernetes_cluster.k8s.kube_config[0].host
     client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].client_key)
