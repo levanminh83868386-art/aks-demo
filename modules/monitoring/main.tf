1689790e30326f55ac2bd0903da93a407f5b64e5
lvanminh <minhle@ntdatateam.onmicrosoft.com>
2025-10-29 13:07:15 +0700

    s

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -1,20 +1,20 @@
-# # monitoring.tf - Deploy the Helm chart
-# resource "helm_release" "kube_prometheus_stack" {
-#   # Helm repository details
-#   name       = "kube-prometheus-stack"
-#   repository = "https://prometheus-community.github.io/helm-charts"
-#   chart      = "kube-prometheus-stack"
-#   version    = "59.0.0" # Use a stable, recent version
-#   namespace  = "monitoring"
-#   create_namespace = true
+# monitoring.tf - Deploy the Helm chart
+resource "helm_release" "kube_prometheus_stack" {
+  # Helm repository details
+  name       = "kube-prometheus-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = "59.0.0" # Use a stable, recent version
+  namespace  = "monitoring"
+  create_namespace = true
   
-#   values = [
-#     templatefile("${path.module}/values.yaml", {
-#       ip = var.ingress_controller_ip
-#     })
-#   ]
+  values = [
+    templatefile("${path.module}/values.yaml", {
+      ip = "111.11.11.1"
+    })
+  ]
 
-#   # Optional: Wait for all resources to be ready (can take a few minutes)
-#   timeout = 600
-#   wait    = true
-# }
+  # Optional: Wait for all resources to be ready (can take a few minutes)
+  timeout = 600
+  wait    = true
+}
