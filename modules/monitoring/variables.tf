b08a9f6279e156f33ada450ce899749bf1e11676
lvanminh <minhle@ntdatateam.onmicrosoft.com>
2025-10-29 13:02:20 +0700

    ádasdad

--- modules/monitoring/variables.tf
+++ modules/monitoring/variables.tf
@@ -1,24 +1,24 @@
-variable "host" {
-  type        = string
-  description = "aks cluster host"
-}
+# variable "host" {
+#   type        = string
+#   description = "aks cluster host"
+# }
 
-variable "client_certificate" {
-  type        = string
-  description = "aks cluster client certificate"
-}
+# variable "client_certificate" {
+#   type        = string
+#   description = "aks cluster client certificate"
+# }
 
-variable "client_key" {
-  type        = string
-  description = "aks cluster client key"
-}
+# variable "client_key" {
+#   type        = string
+#   description = "aks cluster client key"
+# }
 
-variable "cluster_ca_certificate" {
-  type        = string
-  description = "aks cluster cluster ca certificate"
-}
+# variable "cluster_ca_certificate" {
+#   type        = string
+#   description = "aks cluster cluster ca certificate"
+# }
 
-variable "ingress_controller_ip" {
-  type        = string
-  description = "Public IP of the NGINX Ingress Controller"
-}
+# variable "ingress_controller_ip" {
+#   type        = string
+#   description = "Public IP of the NGINX Ingress Controller"
+# }
