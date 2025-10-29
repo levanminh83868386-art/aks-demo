f6a70d1cfe7ebf891d12230d84fae7300180f06f
lvanminh <minhle@ntdatateam.onmicrosoft.com>
2025-10-29 17:15:01 +0700

    đasadad

--- modules/ingress/main.tf
+++ modules/ingress/main.tf
@@ -1,44 +1,23 @@
-resource "helm_release" "nginx_ingress" {
-  name       = "nginx-ingress"
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  namespace  = "ingress-nginx"
-  create_namespace = true
 
-  values = [
-    file("${path.module}/values.yaml")
-  ]
+# Apply the ingress-nginx manifest using kubectl
+resource "null_resource" "apply_ingress_nginx" {
+  provisioner "local-exec" {
+    command = "kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.7.1/deploy/static/provider/cloud/deploy.yaml"
+  }
 }
 
+# Wait for the service to be created
+data "kubernetes_service" "ingress_nginx" {
+  depends_on = [null_resource.apply_ingress_nginx]
 
-# resource "null_resource" "wait_for_ingress_ip" {
-#   provisioner "local-exec" {
-#     command = <<EOT
-#     for i in {1..30}; do
-#       IP=$(kubectl get svc -n ingress-nginx nginx-ingress-ingress-nginx-controller -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-#       if [ ! -z "$IP" ]; then
-#         echo "Ingress Controller IP: $IP"
-#         echo $IP > ingress_ip.txt
-#         exit 0
-#       fi
-#       echo "Waiting for Ingress IP..."
-#       sleep 10
-#     done
-#     echo "Failed to get Ingress IP after waitinggggg."
-#     exit 1
-#     EOT
-#     interpreter = ["/bin/bash", "-c"]
-#   }
-
-#   depends_on = [helm_release.nginx_ingress]
-# }
-
-
-data "kubernetes_service" "nginx_ingress" {
   metadata {
-    name      = "nginx-ingress-ingress-nginx-controller"
+    name      = "ingress-nginx-controller"
     namespace = "ingress-nginx"
   }
-
-  depends_on = [helm_release.nginx_ingress]
 }
+
+# # Output the external IP
+# output "ingress_nginx_ip" {
+#   value = data.kubernetes_service.ingress_nginx.status[0].load_balancer[0].ingress[0].ip
+#   description = "External IP of the NGINX Ingress Controller"
+# }
